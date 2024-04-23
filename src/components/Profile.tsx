d364602e3098158cd3c255509b0f5330012d38b2
Sahil Metalwala <homefolder@Sahils-MacBook-Pro.local>
2024-04-22 19:45:40 -0500

    feat: add home and profile page frameworks

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+//import { useState } from "react";
+import "../App.css"
+import profilePic from "../assets/harleydarling.jpeg"
+
+const Profile = () => {
+    return (
+        <div className="Profile">
+            <ul><img
+            src={profilePic}
+            width="250"
+            height="225"
+            className="profilePic"
+            alt=""
+          /></ul>
+          <text className="profileUsername">Username</text>
+          <text className="profileStats">4 Posts - 48 Followers - 53 Following</text>
+          <text className="Bio">Lorium Ipsum Set Dolor Amet</text>
+        </div>
+    )
+}
+
+export default Profile;
